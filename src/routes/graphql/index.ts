45954b9876476dbdb5278e1cba1beda73b19b672
VelderGoose <vlniiv@mail.ru>
2024-03-03 19:03:52 +0700

    feat: implement graphql query

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { createGqlResponseSchema, gqlResponseSchema, schemaGQL } from './schemas.js';
+import { graphql } from "graphql";
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +13,10 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      // const source = "{ " + req.body.query + " }";
+      const response = await graphql({schema: schemaGQL, source: /*source*/ req.body.query, 
+        contextValue: fastify.prisma});
+      return response;
     },
   });
 };
